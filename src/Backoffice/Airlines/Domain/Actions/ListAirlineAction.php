975a698269c584e13503e0407aebcb4e126604d2
mmamrutlt <mauricio.mamrut@lightit.io>
2025-03-03 12:51:31 -0300

    fix: add missing types

--- src/Backoffice/Airlines/Domain/Actions/ListAirlineAction.php
+++ src/Backoffice/Airlines/Domain/Actions/ListAirlineAction.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Lightit\Backoffice\Airlines\Domain\Actions;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Lightit\Backoffice\Airlines\Domain\Models\Airline;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class ListAirlineAction
 {
+    /**
+     * @return LengthAwarePaginator<Model>
+     */
     public function execute(): LengthAwarePaginator
     {
         return QueryBuilder::for(Airline::class)
--- src/Backoffice/Cities/Domain/Actions/ListCityAction.php
+++ src/Backoffice/Cities/Domain/Actions/ListCityAction.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Lightit\Backoffice\Cities\Domain\Actions;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Lightit\Backoffice\Cities\Domain\Models\City;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class ListCityAction
 {
+    /**
+     * @return LengthAwarePaginator<Model>
+     */
     public function execute(): LengthAwarePaginator
     {
         return QueryBuilder::for(City::class)
