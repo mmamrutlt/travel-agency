ddfc892549a10162d0666e9a2f194f4c12857647
mmamrutlt <mauricio.mamrut@lightit.io>
2025-03-07 10:15:27 -0300

    fix: add querybuilder to list actions

--- src/Backoffice/Airlines/Domain/Actions/ListAirlineAction.php
+++ src/Backoffice/Airlines/Domain/Actions/ListAirlineAction.php
@@ -4,41 +4,30 @@ declare(strict_types=1);
 
 namespace Lightit\Backoffice\Airlines\Domain\Actions;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Lightit\Backoffice\Airlines\Domain\Models\Airline;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ListAirlineAction
 {
-    public function __construct(
-        private readonly Request $request,
-    ) {
-    }
-
     /**
      * @return LengthAwarePaginator<Airline>
      */
     public function execute(): LengthAwarePaginator
     {
-        $query = Airline::with(['flights']);
-
-        if ($this->request->has('filter.name')) {
-            $query->where('name', 'like', '%' . $this->request->string('filter.name') . '%');
-        }
-
-        if ($this->request->has('filter.city')) {
-            $query->whereHas('flights', function (Builder $query) {
-                $query->where('departure_city_id', $this->request->input('filter.city'))
-                    ->orWhere('arrival_city_id', $this->request->input('filter.city'));
-            });
-        }
-
-        if ($this->request->has('sort')) {
-            $direction = $this->request->string('direction', 'asc')->toString();
-            $query->orderBy($this->request->string('sort')->toString(), $direction);
-        }
-
-        return $query->paginate(10);
+        return QueryBuilder::for(Airline::class)
+            ->allowedFilters([
+                'name',
+                AllowedFilter::callback('city', function ($query, $value) {
+                    $query->whereHas('flights', function ($query) use ($value) {
+                        $query->where('departure_city_id', $value)
+                            ->orWhere('arrival_city_id', $value);
+                    });
+                })
+            ])
+            ->allowedSorts(['name'])
+            ->with(['flights'])
+            ->paginate(10);
     }
 }
--- src/Backoffice/Cities/Domain/Actions/ListCityAction.php
+++ src/Backoffice/Cities/Domain/Actions/ListCityAction.php
@@ -4,33 +4,22 @@ declare(strict_types=1);
 
 namespace Lightit\Backoffice\Cities\Domain\Actions;
 
-use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Lightit\Backoffice\Cities\Domain\Models\City;
-
+use Spatie\QueryBuilder\QueryBuilder;
 class ListCityAction
 {
-    public function __construct(
-        private readonly Request $request,
-    ) {
-    }
-
     /**
      * @return LengthAwarePaginator<City>
      */
     public function execute(): LengthAwarePaginator
     {
-        $query = City::with(['departureFlights', 'arrivalFlights']);
-
-        if ($this->request->has('filter.name')) {
-            $query->where('name', 'like', '%' . $this->request->string('filter.name') . '%');
-        }
-
-        if ($this->request->has('sort')) {
-            $direction = $this->request->string('direction', 'asc')->toString();
-            $query->orderBy($this->request->string('sort')->toString(), $direction);
-        }
-
-        return $query->paginate(10);
+        return QueryBuilder::for(City::class)
+            ->allowedFilters([
+                'name',
+            ])
+            ->allowedSorts(['name'])
+            ->with(['departureFlights', 'arrivalFlights'])
+            ->paginate(10);
     }
 }
